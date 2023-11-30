61bb126f5298d149ffae81df8e4e8e917447d54e
veromihaly <veromihaly18@gmail.com>
2023-11-30 13:17:51 +0100

    Created CarServiceImpl and OwnerServiceImpl for service interfaces

---
+++ src/main/java/project/service/impl/CarServiceImpl.java
@@ -0,0 +1,50 @@
+package project.service.impl;
+
+/**
+ * @author s727953
+ *
+ */
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import project.entity.CarDTO;
+import project.repository.CarRepo;
+import project.service.CarService;
+import project.uito.CarUITO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Transactional
+public class CarServiceImpl implements CarService {
+    @Autowired
+    private CarRepo carRepo;
+
+    @Override
+    public List<CarUITO> getAllCar() {
+        List<CarUITO> carUITOLst = new ArrayList<>();
+        List<CarDTO> carDTOLst = carRepo.findAll();
+
+        carDTOLst.forEach(dto -> {
+            CarUITO tmpUiTO = new CarUITO();
+
+            BeanUtils.copyProperties(dto, tmpUiTO);
+            carUITOLst.add(tmpUiTO);
+        });
+
+        return carUITOLst;
+    }
+
+    @Override
+    public CarUITO getCar(CarUITO carUITO) {
+        System.out.println(">>>>> "+carUITO.getCarBrand());
+        CarDTO dto = carRepo.findTitleByCarBrand(carUITO.getCarBrand());
+        CarUITO uito = new CarUITO();
+
+        BeanUtils.copyProperties(dto, uito);
+        return uito;
+    }
+}
