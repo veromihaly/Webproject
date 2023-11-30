407c685cd7d468b16dbf76853fcf83484e29df8d
veromihaly <veromihaly18@gmail.com>
2023-11-30 13:32:30 +0100

    Added all the necessary utils for the project

---
+++ src/main/java/project/utills/CarConverter.java
@@ -0,0 +1,46 @@
+package project.utills;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import project.service.CarService;
+import project.uito.CarUITO;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+
+@Service
+public class CarConverter implements Converter {
+
+    @Autowired
+    private CarService carService;
+
+    private static final Logger LOG = LoggerFactory.getLogger(CarConverter.class.getName());
+
+    @Override
+    public Object getAsObject(FacesContext fc, UIComponent uic, String string) {
+        LOG.info("getAsObject: " + carService);
+
+        CarUITO carUITO = new CarUITO();
+        carUITO.setCarBrand(string);
+        carUITO = carService.getCar(carUITO);
+        System.out.println(carUITO.toString());
+        return carUITO;
+    }
+
+    @Override
+    public String getAsString(FacesContext fc, UIComponent uic, Object obj) {
+        LOG.info("getAsString obj class: " + obj.getClass().getName());
+        if (obj instanceof CarUITO) {
+            CarUITO dept = (CarUITO) obj;
+            LOG.info("getAsString def name: " + dept.getCarBrand());
+            return dept.getCarBrand();
+        } else {
+            StringBuilder sbError = new StringBuilder("The object of class ");
+            sbError.append(obj.getClass().getName()).append(" is not of CarUITO");
+            throw new ClassCastException(sbError.toString());
+        }
+    }
+}
